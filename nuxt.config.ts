d908913fbb42a01d4be09a489154b37ceebe8dc3
Ugbor Chinedum <38799833+CharlesChinedum@users.noreply.github.com>
2023-09-15 12:38:52 +0000

    fix: made the fallback and preference theme: light

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,8 +11,8 @@ export default defineNuxtConfig({
   modules: ["@bootstrap-vue-next/nuxt", "@nuxtjs/color-mode"],
   colorMode: {
     classSuffix: "",
-    preference: "dark", // default value of $colorMode.preference
-    fallback: "dark", // fallback value if not system preference found
+    preference: "light", // default value of $colorMode.preference
+    fallback: "light", // fallback value if not system preference found
   },
   css: [
     "~/assets/css/main.css",
